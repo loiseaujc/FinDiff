ce45776b01c717a58c0048ea469e8453348c4745
Jean-Christophe <loiseau.jc@gmail.com>
2025-11-18 15:38:31 +0100

    Forward and backward finite difference schemes.

--- app/main.f90
+++ app/main.f90
@@ -1,15 +1,43 @@
 program main
    use FinDiff
    implicit none
-   integer(ilp), parameter :: n = 8
    integer(ilp) :: order
-   real(wp), allocatable :: stencil(:)
+   real(wp), allocatable :: weights(:)
 
-   do order = 2, n, 2
-      stencil = central_findiff(order)
+   !-----------------------------------------------------
+   !-----     Central Finite Difference Schemes     -----
+   !-----------------------------------------------------
+
+   do order = 2, 8, 2
+      weights = central_findiff(order)
       print *, "Central finite difference scheme for the first derivative"
       print *, "     - Order  :", order
-      print *, "     - Stencil:", stencil
+      print *, "     - Stencil:", weights
       print *, ""
    end do
+
+   !------------------------------------------------------
+   !-----     Backward Finite Difference Schemes     -----
+   !------------------------------------------------------
+
+   do order = 1, 4
+      weights = backward_findiff(order)
+      print *, "Backward finite difference scheme for the first derivative"
+      print *, "    - Order  :", order
+      print *, "    - Stencil:", weights
+      print *, ""
+   end do
+
+   !-----------------------------------------------------
+   !-----     Forward Finite Difference Schemes     -----
+   !-----------------------------------------------------
+
+   do order = 1, 4
+      weights = forward_findiff(order)
+      print *, "Forward finite difference scheme for the first derivative"
+      print *, "    - Order  :", order
+      print *, "    - Stencil:", weights
+      print *, ""
+   end do
+
 end program main
--- src/FinDiff.f90
+++ src/FinDiff.f90
@@ -6,27 +6,66 @@ module FinDiff
    private
 
    public :: ilp, wp
+   public :: taylor_optimized_findiff
    public :: central_findiff
+   public :: backward_findiff
+   public :: forward_findiff
 
    interface
-      module function central_findiff(order) result(stencil)
+      module function central_findiff(order) result(weights)
          integer(ilp), intent(in) :: order
-         real(wp), allocatable :: stencil(:)
+         real(wp), allocatable :: weights(:)
       end function central_findiff
+
+      module function backward_findiff(order) result(weights)
+         integer(ilp), intent(in) :: order
+         real(wp), allocatable :: weights(:)
+      end function backward_findiff
+
+      module function forward_findiff(order) result(weights)
+         integer(ilp), intent(in) :: order
+         real(wp), allocatable :: weights(:)
+      end function forward_findiff
+
+      module function taylor_optimized_findiff(stencil) result(weights)
+         integer(ilp), intent(in) :: stencil(:)
+         real(wp), allocatable :: weights(:)
+      end function taylor_optimized_findiff
    end interface
 
 contains
 
+   module procedure taylor_optimized_findiff
+   real(wp), allocatable :: A(:, :), b(:)
+   !> Linear system.
+   A = transpose(vandermonde(real(stencil, kind=wp)))
+   allocate (b(size(A, 1)), source=0.0_wp); b(2) = 1.0_wp
+   !> Finite-difference weights.
+   weights = solve(A, b)
+   end procedure taylor_optimized_findiff
+
    module procedure central_findiff
    integer(ilp) :: i, n
-   real(wp), allocatable :: A(:, :), b(:)
    !> Number of points in the stencil.
    n = order + 1
-   !> Linear system.
-   A = transpose(vandermonde([(real(i, kind=wp), i=(-n + 1)/2, (n - 1)/2)]))
-   allocate (b(size(A, 1)), source=0.0_wp); b(2) = 1.0_wp
-   !> Solve linear system.
-   stencil = solve(A, b)
+   !> Finite-difference weights
+   weights = taylor_optimized_findiff([(i, i=(-n + 1)/2, (n - 1)/2)])
    end procedure central_findiff
+
+   module procedure backward_findiff
+   integer(ilp) :: i, n
+   !> Number of points in the stencil.
+   n = order + 1
+   !> Finite-difference weights.
+   weights = taylor_optimized_findiff([(i, i=-n + 1, 0)])
+   end procedure backward_findiff
+
+   module procedure forward_findiff
+   integer(ilp) :: i, n
+   !> Number of points in the stencil.
+   n = order + 1
+   !> Finite-difference weights.
+   weights = taylor_optimized_findiff([(i, i=0, n - 1)])
+   end procedure forward_findiff
 
 end module FinDiff
